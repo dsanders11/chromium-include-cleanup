d94d8351f7c14796e96dfd9232ef81b9909b3766
David Sanders <dsanders11@ucsbalum.com>
2022-01-31 01:35:03 -0800

    feat: mostly functional find_unused_edges.py

--- find_unused_edges.py
+++ find_unused_edges.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import argparse
+import asyncio
 import csv
 import logging
 import pathlib
@@ -9,24 +10,27 @@ import sys
 from typing import Callable, List, Tuple
 
 from tqdm import tqdm
+from tqdm.contrib.logging import logging_redirect_tqdm
 
 # Insert this script's directory into the path so it can import sibling modules
 # TODO - Is this actually necessary?
 sys.path.insert(0, pathlib.Path(__file__).parent.resolve())
 
-from clangd_lsp import get_unused_includes
+from clangd_lsp import ClangdClient
 from include_analysis import parse_raw_include_analysis_output
 
 
-def find_unused_edges(
-    include_analysis: dict, progress_callback: Callable[[str], None] = None
+async def find_unused_edges(
+    clangd_client: ClangdClient,
+    include_analysis: dict,
+    progress_callback: Callable[[str], None] = None,
 ) -> List[Tuple[str, str, int]]:
     """Finds unused edges according to clangd and returns a list of [includer, included, asize]"""
 
     unused_edges = []
 
     for root in include_analysis["roots"]:
-        for unused_include in get_unused_includes(root):
+        for unused_include in await clangd_client.get_unused_includes(root):
             try:
                 unused_edges.append(
                     (
@@ -46,7 +50,9 @@ def find_unused_edges(
     return unused_edges
 
 
-def main():
+# TODO - Ctrl+C doesn't cleanly exit
+# TODO - How to detect when compilation DB isn't found and clangd is falling back (won't work)
+async def main():
     parser = argparse.ArgumentParser(
         description="Find unused edges, guided by the JSON analysis data from analyze_includes.py"
     )
@@ -56,8 +62,19 @@ def main():
         help="The include analysis output to use (- for stdin).",
     )
     parser.add_argument(
-        "--root-filter", help="Regex to filter which root files are analyzed."
+        "--clangd-path",
+        type=pathlib.Path,
+        help="Path to the clangd executable to use.",
+        default="clangd.exe" if sys.platform == "win32" else "clangd",
     )
+    parser.add_argument(
+        "--chromium-src", type=pathlib.Path, help="Path to the Chromium source tree.", default=pathlib.Path(".")
+    )
+    parser.add_argument(
+        "--compile-commands-dir", type=pathlib.Path, help="Specify a path to look for compile_commands.json."
+    )
+    parser.add_argument("--root-filter", help="Regex to filter which root files are analyzed.")
+    parser.add_argument("--verbose", action="store_true", default=False, help="Enable verbose logging.")
     args = parser.parse_args()
 
     try:
@@ -66,13 +83,18 @@ def main():
         print("error: --root-filter is not a valid regex")
         return 1
 
-    include_analysis = parse_raw_include_analysis_output(
-        args.include_analysis_output.read(), root_filter=root_filter
-    )
+    if args.compile_commands_dir and not args.compile_commands_dir.is_dir():
+        print("error: --compile-commands-dir must be a directory")
+        return 1
+
+    include_analysis = parse_raw_include_analysis_output(args.include_analysis_output.read(), root_filter=root_filter)
 
     if not include_analysis:
-        print("Could not process include analysis output file")
-        return 1
+        print("error: Could not process include analysis output file")
+        return 2
+
+    if args.verbose:
+        logging.basicConfig(level=logging.DEBUG)
 
     # This script only needs a few of the keys from include analysis
     include_analysis = {
@@ -80,20 +102,43 @@ def main():
         "esizes": include_analysis["esizes"],
     }
 
-    progress_output = tqdm(total=len(include_analysis["roots"]))
-    progress_output.display()
-    unused_edges = find_unused_edges(
-        include_analysis, progress_callback=lambda _: progress_output.update()
-    )
-    progress_output.close()
+    root_path = args.chromium_src.resolve()
 
-    # Dump unused edges as CSV output on stdout
-    sys.stdout.reconfigure(newline="")
-    csv_writer = csv.writer(sys.stdout)
-    csv_writer.writerows(unused_edges)
+    if not ClangdClient.validate_config(root_path):
+        print("error: Must have a .clangd config with IncludeCleaner enabled")
+        return 3
+
+    clangd_client = ClangdClient(
+        args.clangd_path,
+        root_path,
+        args.compile_commands_dir.resolve() if args.compile_commands_dir else None,
+    )
+
+    unused_edges = None
+
+    try:
+        progress_output = tqdm(total=len(include_analysis["roots"]))
+        await clangd_client.start()
+
+        with logging_redirect_tqdm():
+            progress_output.display()
+            unused_edges = await find_unused_edges(
+                clangd_client,
+                include_analysis,
+                progress_callback=lambda _: progress_output.update(),
+            )
+    finally:
+        progress_output.close()
+        await clangd_client.exit()
+
+    if unused_edges:
+        # Dump unused edges as CSV output on stdout
+        sys.stdout.reconfigure(newline="")
+        csv_writer = csv.writer(sys.stdout)
+        csv_writer.writerows(unused_edges)
 
     return 0
 
 
 if __name__ == "__main__":
-    sys.exit(main())
+    sys.exit(asyncio.run(main()))

046117053a76dec9eec5d2cc1553e5b188419def
David Sanders <dsanders11@ucsbalum.com>
2022-03-20 19:08:20 -0700

    feat: move ignores to JSON config files

--- filter_include_changes.py
+++ filter_include_changes.py
@@ -9,6 +9,8 @@ import re
 import sys
 from typing import List, Tuple
 
+from pydantic import BaseModel
+
 # Insert this script's directory into the path so it can import sibling modules
 # TODO - Is this actually necessary?
 sys.path.insert(0, pathlib.Path(__file__).parent.resolve())
@@ -17,140 +19,30 @@ from common import IncludeChange
 
 Change = Tuple[IncludeChange, int, str, str, int]
 
+
+class IgnoresSubConfiguration(BaseModel):
+    filenames: List[str] = []
+    headers: List[str] = []
+    edges: List[Tuple[str, str]] = []
+
+
+class IgnoresConfiguration(BaseModel):
+    add: IgnoresSubConfiguration = IgnoresSubConfiguration()
+    remove: IgnoresSubConfiguration = IgnoresSubConfiguration()
+
+
 GENERATED_FILE_REGEX = re.compile(r"^out/\w+/gen/.*$")
 MOJOM_HEADER_REGEX = re.compile(r"^.*.mojom[^.]*.h$")
-
-# This is a list of known filenames to skip when checking for unused
-# includes. It's mostly a list of umbrella headers where the includes
-# will appear to clangd to be unused, but are meant to be included.
-UNUSED_INCLUDE_FILENAME_SKIP_LIST = (
-    "base/trace_event/base_tracing.h",
-    "mojo/public/cpp/system/core.h",
-    # TODO - Keep populating this list
-)
-
-# This is a list of known filenames where clangd produces a false
-# positive when suggesting as unused includes to remove. Usually these
-# are umbrella headers, or headers where clangd thinks the canonical
-# location for a symbol is actually in a forward declaration, causing
-# it to flag the correct header as unused everywhere, so ignore those.
-UNUSED_INCLUDE_IGNORE_LIST = (
-    "base/allocator/buildflags.h",
-    "base/bind.h",
-    "base/callback.h",
-    "base/clang_profiling_buildflags.h",
-    "base/compiler_specific.h",
-    "base/hash/md5.h",
-    "base/strings/string_piece.h",
-    "base/trace_event/base_tracing.h",
-    "build/branding_buildflags.h",
-    "build/build_config.h",
-    "build/chromecast_buildflags.h",
-    "build/chromeos_buildflags.h",
-    "chrome/browser/ui/browser.h",
-    "chrome/browser/ui/browser_list.h",
-    "chrome/common/buildflags.h",
-    "components/safe_browsing/buildflags.h",
-    "components/signin/public/base/signin_buildflags.h",
-    "content/browser/renderer_host/render_frame_host_impl.h",
-    "content/browser/web_contents/web_contents_impl.h",
-    "extensions/buildflags/buildflags.h",
-    "extensions/renderer/extension_frame_helper.h",
-    "media/media_buildflags.h",
-    "mojo/public/cpp/bindings/pending_receiver.h",
-    "mojo/public/cpp/bindings/pending_remote.h",
-    "mojo/public/cpp/bindings/receiver.h",
-    "mojo/public/cpp/bindings/remote.h",
-    "mojo/public/cpp/system/core.h",
-    "ppapi/buildflags/buildflags.h",
-    "printing/buildflags/buildflags.h",
-    "third_party/blink/renderer/platform/graphics/paint/paint_filter.h",
-    "ui/aura/window.h",
-    "ui/gfx/image/image_skia_rep.h",
-    "v8/include/v8.h",
-    # TODO - Keep populating this list
-)
-
-# This is a list of known filenames where clangd produces a false
-# positive when suggesting as includes to add.
-ADD_INCLUDE_IGNORE_LIST: Tuple[str, ...] = (
-    "absl/base/internal/inline_variable.h",
-    "absl/types/internal/optional.h",
-    "absl/types/optional.h",
-    "base/atomicops_internals_portable.h",
-    "base/bind_internal.h",
-    "base/callback_forward.h",
-    "base/callback_internal.h",
-    "base/hash/md5_constexpr_internal.h",
-    "base/metrics/histogram_macros_internal.h",
-    "base/numerics/safe_conversions_impl.h",
-    "base/numerics/safe_math_clang_gcc_impl.h",
-    "base/numerics/safe_math_shared_impl.h",
-    "base/strings/string_piece_forward.h",
-    "base/trace_event/trace_event_impl.h",
-    "ui/base/metadata/metadata_macros_internal.h",
-    "ui/gfx/image/image_skia_rep_default.h",
-    "v8-internal.h",
-    # TODO - Keep populating this list
-)
-
-UNUSED_EDGE_IGNORE_LIST = (
-    (
-        "base/allocator/partition_allocator/address_pool_manager.h",
-        "base/allocator/partition_allocator/address_pool_manager_bitmap.h",
-    ),
-    ("base/atomicops.h", "base/atomicops_internals_portable.h"),
-    ("base/debug/crash_logging.h", "base/strings/string_number_conversions.h"),
-    ("base/hash/md5_constexpr.h", "base/hash/md5_constexpr_internal.h"),
-    ("base/memory/aligned_memory.h", "base/bits.h"),
-    ("base/numerics/safe_math_shared_impl.h", "base/numerics/safe_math_clang_gcc_impl.h"),
-    ("base/trace_event/typed_macros.h", "base/tracing/protos/chrome_track_event.pbzero.h"),
-    ("chrome/browser/ui/browser.h", "chrome/browser/ui/signin_view_controller.h"),
-    (
-        "components/download/public/common/download_item_rename_progress_update.h",
-        "components/enterprise/common/download_item_reroute_info.h",
-    ),
-    ("ipc/ipc_message_macros.h", "base/task/common/task_annotator.h"),
-    ("media/base/ipc/media_param_traits.h", "media/base/ipc/media_param_traits_macros.h"),
-    ("mojo/public/cpp/bindings/lib/serialization.h", "mojo/public/cpp/bindings/array_traits_stl.h"),
-    ("mojo/public/cpp/bindings/lib/serialization.h", "mojo/public/cpp/bindings/map_traits_stl.h"),
-    (
-        "third_party/blink/renderer/core/frame/web_feature.h",
-        "third_party/blink/public/mojom/web_feature/web_feature.mojom-blink.h",
-    ),
-    ("third_party/blink/renderer/core/page/page.h", "third_party/blink/public/mojom/page/page.mojom-blink.h"),
-    (
-        "third_party/blink/renderer/core/probe/core_probes.h",
-        "third_party/blink/renderer/core/core_probes_inl.h",
-    ),
-    (
-        "third_party/blink/renderer/platform/wtf/allocator/allocator.h",
-        "base/allocator/partition_allocator/partition_alloc.h",
-    ),
-    (
-        "third_party/blink/renderer/platform/wtf/hash_table.h",
-        "third_party/blink/renderer/platform/wtf/hash_iterators.h",
-    ),
-    (
-        "third_party/blink/renderer/platform/wtf/text/atomic_string.h",
-        "third_party/blink/renderer/platform/wtf/text/string_concatenate.h",
-    ),
-    ("services/network/public/cpp/url_request_mojom_traits.h", "services/network/public/cpp/resource_request.h"),
-    ("skia/config/SkUserConfig.h", "skia/ext/skia_histogram.h"),
-    ("ui/gfx/image/image_skia_rep.h", "ui/gfx/image/image_skia_rep_default.h"),
-    ("ui/gfx/image/image_skia_rep_default.h", "cc/paint/paint_op_buffer.h"),
-    # TODO - Keep populating this list
-)
 
 
 def filter_changes(
     changes: List[Change],
+    ignores: IgnoresConfiguration = None,
     filename_filter: re.Pattern = None,
     header_filter: re.Pattern = None,
     change_type_filter: IncludeChange = None,
     filter_generated_files=True,
     filter_mojom_headers=True,
-    filter_false_positives=True,
 ):
     """Filter changes"""
 
@@ -174,22 +66,29 @@ def filter_changes(
         if filter_mojom_headers and MOJOM_HEADER_REGEX.match(header):
             continue
 
-        # Cut down on noise by ignoring known false positives
-        if filter_false_positives:
+        # Cut down on noise by using ignores
+        if ignores:
             if change_type is IncludeChange.REMOVE:
-                if filename in UNUSED_INCLUDE_FILENAME_SKIP_LIST:
+                if filename in ignores.remove.filenames:
                     logging.info(f"Skipping filename for unused includes: {filename}")
                     continue
 
-                ignore_edge = (filename, header) in UNUSED_EDGE_IGNORE_LIST
-                ignore_include = header in UNUSED_INCLUDE_IGNORE_LIST
+                ignore_edge = (filename, header) in ignores.remove.edges
+                ignore_include = header in ignores.remove.headers
 
                 # TODO - Ignore unused suggestion if the include is for the associated header
 
                 if ignore_edge or ignore_include:
                     continue
             elif change_type is IncludeChange.ADD:
-                if header in ADD_INCLUDE_IGNORE_LIST:
+                if filename in ignores.add.filenames:
+                    logging.info(f"Skipping filename for adding includes: {filename}")
+                    continue
+
+                ignore_edge = (filename, header) in ignores.add.edges
+                ignore_include = header in ignores.add.headers
+
+                if ignore_edge or ignore_include:
                     continue
 
         yield (change_type_value, line, filename, header, *_)
@@ -204,9 +103,10 @@ def main():
     )
     parser.add_argument("--filename-filter", help="Regex to filter which files have changes outputted.")
     parser.add_argument("--header-filter", help="Regex to filter which headers are included in the changes.")
+    parser.add_argument("--ignores", default="chromium", help="Name of ignores file to use.")
     parser.add_argument("--no-filter-generated-files", action="store_true", help="Don't filter out generated files.")
     parser.add_argument("--no-filter-mojom-headers", action="store_true", help="Don't filter out mojom headers.")
-    parser.add_argument("--no-filter-false-positives", action="store_true", help="Don't filter out known false positives.")
+    parser.add_argument("--no-filter-ignores", action="store_true", help="Don't filter out ignores.")
     group = parser.add_mutually_exclusive_group()
     group.add_argument("--add-only", action="store_true", default=False, help="Only output includes to add.")
     group.add_argument("--remove-only", action="store_true", default=False, help="Only output includes to remove.")
@@ -235,17 +135,28 @@ def main():
     else:
         change_type_filter = None
 
+    if args.no_filter_ignores:
+        ignores = None
+    else:
+        ignores_config_file = pathlib.Path(__file__).parent.joinpath("ignores", args.ignores).with_suffix(".json")
+
+        if not ignores_config_file.exists():
+            print(f"error: no ignores config file found: {ignores_config_file}")
+            return 1
+
+        ignores = IgnoresConfiguration.parse_file(ignores_config_file)
+
     csv_writer = csv.writer(sys.stdout)
 
     try:
         for change in filter_changes(
             csv.reader(args.changes_file),
+            ignores=ignores,
             filename_filter=filename_filter,
             header_filter=header_filter,
             change_type_filter=change_type_filter,
             filter_generated_files=not args.no_filter_generated_files,
             filter_mojom_headers=not args.no_filter_mojom_headers,
-            filter_false_positives=not args.no_filter_false_positives,
         ):
             csv_writer.writerow(change)
 
